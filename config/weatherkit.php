713edcd8189762ac9c1bfe05e7602f05e75ae116
Blake Krone <blakekrone@gmail.com>
2022-12-31 14:09:53 -0600

    removed base_path()

--- config/weatherkit.php
+++ config/weatherkit.php
@@ -4,7 +4,7 @@ return [
         'config' => [
             'jwt' => env('WEATHERKIT_JWT_TOKEN', ''),
 
-            'key' => base_path(env('WEATHERKIT_KEY', '')),
+            'key' => env('WEATHERKIT_KEY', ''),
             'keyId' => env('WEATHERKIT_KEY_ID', ''),
             'teamId' => env('WEATHERKIT_TEAM_ID'),
             'bundleId' => env('WEATHERKIT_BUNDLE_ID'),
